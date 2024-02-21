68ad1c353df0268af8f2e784ad3a6e2702c3c539
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-21 16:03:47 +0500

    Add files via upload

---
+++ Ex10/fvtn.ts
@@ -0,0 +1,5 @@
+let FavNumber = 10;  // this is favourite  number of user 10 
+
+let message = "My favourite Number is";
+
+console.log(message, FavNumber);  // this is to print  all thing which  you write in the console  
