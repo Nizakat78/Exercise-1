2b1e6b654decde373186bd749c004b40e7f35119
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-22 15:41:34 +0500

    Add files via upload

---
+++ Ex13/ride.js
@@ -0,0 +1,2 @@
+var transportName = ["Motor", "Cycle", "Train", "Car"];
+transportName.forEach(function (Ridename) { return console.log("I would like to own a ".concat(Ridename, " motorcycle.")); });
---
+++ Ex13/ride.ts
@@ -0,0 +1,4 @@
+let transportName =["Motor", "Cycle", "Train", "Car"];
+
+transportName.forEach(Ridename => console.log(`I would like to own a ${Ridename} motorcycle.`));
+
