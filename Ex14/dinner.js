172f2f2a592b76436bced165ba169e863d3799a6
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-22 15:47:59 +0500

    Add files via upload

---
+++ Ex14/dinner.js
@@ -0,0 +1,2 @@
+var guestinvite = ["Ahmed", "ALi", "Alireza", "Mohammad"];
+guestinvite.forEach(function (guestname) { return console.log("Assalam-e-Alaikum ".concat(guestname, " who would you invite?")); });
---
+++ Ex14/dinner.ts
@@ -0,0 +1,4 @@
+let guestinvite =["Ahmed", "ALi","Alireza","Mohammad"];
+
+guestinvite.forEach(guestname => console.log(`Assalam-e-Alaikum ${guestname} who would you invite?`));
+
