d0475ce98e213c6a04d5b4107023392b21ae00b8
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-23 10:45:39 +0500

    Add files via upload

---
+++ Ex15/UpdatreGuestlist.js
@@ -0,0 +1,10 @@
+var Guestlist = ["Kmran", "Ahmed", "Ali", "Mohammad"];
+Guestlist.forEach(function (Nizakat) { return console.log("Hello ".concat(Nizakat, " I would like to invite in Dinner.")); });
+var oneguestcantcome = Guestlist[1];
+console.log("".concat(oneguestcantcome, " Can't come today"));
+// Task 2 
+var UpdateGuestlist = "Update Guest's List Here";
+console.log(UpdateGuestlist);
+// Task 03
+Guestlist[1] = "Aslam";
+Guestlist.forEach(function (Ali) { return console.log("Hello ".concat(Ali, " Would like to Invite for dinner.")); });
---
+++ Ex15/UpdatreGuestlist.ts
@@ -0,0 +1,17 @@
+let Guestlist = ["Kmran", "Ahmed", "Ali","Mohammad"];
+
+Guestlist.forEach(Nizakat => console.log(`Hello ${Nizakat} I would like to invite in Dinner.` ));
+
+let oneguestcantcome = Guestlist[1];
+
+console.log(`${oneguestcantcome} Can't come today`);
+
+// Task 2 
+let UpdateGuestlist = "Update Guest's List Here";
+
+console.log(UpdateGuestlist);
+// Task 03
+
+Guestlist[1] = "Aslam";
+
+Guestlist.forEach(Ali=>console.log(`Hello ${Ali} Would like to Invite for dinner.`));
