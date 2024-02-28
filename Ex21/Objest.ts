951efb740a5663d1acd9d57c7448229b958f9428
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-28 16:30:18 +0500

    Add files via upload

---
+++ Ex21/Objest.ts
@@ -0,0 +1,49 @@
+
+
+let K2 = {
+    Mountain_Name : "K2",
+    Location : "Pakistan and China ",
+    Height : 8611
+}
+console.log(`\n Mountain's Details \n`);
+console.log(`\n K2 Details \n`);
+console.log(`Mountain Name:${K2.Mountain_Name}`);
+console.log(` Location: ${K2.Location}`);
+console.log(` Height: ${K2.Height}` +' Meters');
+
+let  Mountain_Everest ={
+    Mountain_Name : "Mount Everest",
+    Location : "Nepal and Tibet",
+    Height:  8849
+}
+console.log(`\n Mount Everest Details \n`)
+console.log  (`Mountain Name: ${Mountain_Everest.Mountain_Name}`)
+console.log(`Location: ${Mountain_Everest.Location}`);
+console.log(`Height: ${Mountain_Everest.Height}`+ `Meters`);
+
+let Tirich_Mir ={
+    Mountain_Name : 'Tirich Mir',
+    Location : 'Chitral District of Khyber Pakhtunkhwa, Pakistan',
+    Height :7708
+}
+console.log(`\n Tirich  Mir Details \n`)
+console.log(`Mountain Name: ${Tirich_Mir.Mountain_Name}`);
+console.log(`Location :${Tirich_Mir.Location}`)
+console.log(`Height : ${Tirich_Mir.Height}' + 'Meters`);
+
+let Broad_Peak ={
+    Peak_Name : "Broad Peak",
+    Location: 'Karakoram range spanning Gilgit-Baltistan, Pakistan and Xinjiang, China.',
+    Height:  8051
+}
+
+console.log(`\n Broad Peak Details\n`);
+console.log(`Peak Name: ${Broad_Peak.Peak_Name}`)
+console.log(`Location :  ${Broad_Peak.Location}`);
+console.log(`Height ${Broad_Peak.Height}`+`Meters`)
+
+let River_Details = { 
+    Riverr_Name: "Riverr-Indus",
+}
+
+let Hello = [ 'River-Indus', 'Basol River', 'Nal River', 'Hub River' ]
---
+++ Ex21/objest.js
@@ -0,0 +1,30 @@
+var K2 = {
+    Mountain_Name: "K2",
+    Location: "Pakistan and China ",
+    Height: 8611
+};
+console.log("\n Mountain's Details \n");
+console.log("\n K2 Details \n");
+console.log("Mountain Name:".concat(K2.Mountain_Name));
+console.log(" Location: ".concat(K2.Location));
+console.log(" Height: ".concat(K2.Height) + ' Meters');
+var Mountain_Everest = {
+    Mountain_Name: "Mount Everest",
+    Location: "Nepal and Tibet",
+    Height: 8849
+};
+console.log("\n Mount Everest Details \n");
+console.log("Mountain Name: ".concat(Mountain_Everest.Mountain_Name));
+console.log("Location: ".concat(Mountain_Everest.Location));
+console.log("Height: ".concat(Mountain_Everest.Height) + "Meters");
+var Tirich_Mir = {
+    Mountain_Name: 'Tirich Mir',
+    Location: 'Chitral District of Khyber Pakhtunkhwa, Pakistan',
+    Height: 7708
+};
+console.log("\n Tirich  Mir Details \n");
+console.log("Mountain Name: ".concat(Tirich_Mir.Mountain_Name));
+console.log("Location :".concat(Tirich_Mir.Location));
+console.log("Height : ".concat(Tirich_Mir.Height, "+ 'Meters"));
+//Mountain list 
+var mountain_List = ["K2", "Mountain Everest", "Tirich Mir", "Broad Peak"];
