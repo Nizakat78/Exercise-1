c7c3eada0d0535891eac8c99417e1447f6129c21
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-28 10:54:12 +0500

    Add files via upload

---
+++ Ex20/Favthings.js
@@ -0,0 +1,24 @@
+//Mountain list 
+var mountain_List = ["K2", "Mountain Everest", "Tirich Mir", "Broad Peak"];
+console.log("\n Here is moutuain list: \n");
+console.log(mountain_List);
+//River list 
+var River_list = ["River-Indus", "Basol River", "Nal River", "Hub River"];
+console.log("\n Here is River List\n");
+console.log(River_list);
+// Here is Countries list
+var Country_Name = ['Pakistan', 'India', 'Iran', 'Bangladesh'];
+console.log("Here is Counties List");
+console.log(Country_Name);
+// Add cities to a list 
+var Favt_citieslist = ["Karachi", "Daharki", "Islamabad", "Lahore"];
+console.log("\n Here is cities List \n");
+console.log(Favt_citieslist);
+// Here is  languages  Name 
+var Language_Name = ['Sindhi', 'Saraki', 'English', 'Urdu', 'Punjabi'];
+console.log("\n Here is Languages List:\n");
+console.log(Language_Name);
+// Here is list  I  Like it 
+var MyFavt_list = ['Apple', 'Computer', 'Visiting', ' Computer Programming'];
+console.log("\n Here is I like the things\n");
+console.log(MyFavt_list);
