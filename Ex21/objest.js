4221df2af30f0dea7f4c4b0089478c831b4dec14
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-28 22:23:09 +0500

    Update objest.js

--- Ex21/objest.js
+++ Ex21/objest.js
@@ -25,6 +25,80 @@ var Tirich_Mir = {
 console.log("\n Tirich  Mir Details \n");
 console.log("Mountain Name: ".concat(Tirich_Mir.Mountain_Name));
 console.log("Location :".concat(Tirich_Mir.Location));
-console.log("Height : ".concat(Tirich_Mir.Height, "+ 'Meters"));
-//Mountain list 
-var mountain_List = ["K2", "Mountain Everest", "Tirich Mir", "Broad Peak"];
+console.log("Height : ".concat(Tirich_Mir.Height, "' + 'Meters"));
+var Broad_Peak = {
+    Peak_Name: "Broad Peak",
+    Location: 'Karakoram range spanning Gilgit-Baltistan, Pakistan and Xinjiang, China.',
+    Height: 8051
+};
+console.log("\n Broad Peak Details\n");
+console.log("Peak Name: ".concat(Broad_Peak.Peak_Name));
+console.log("Location :  ".concat(Broad_Peak.Location));
+console.log("Height ".concat(Broad_Peak.Height) + "Meters");
+var River_Details = {
+    Riverr_Name: "Riverr-Indus",
+    Total_length_of_River: 2900,
+    Annual_Flow: 272
+};
+console.log("\n River's Details\n");
+console.log("\n River-Indus Details\n");
+console.log("River Name: ".concat(River_Details.Riverr_Name));
+console.log("Total Length of River: ".concat(River_Details.Total_length_of_River) + "kilometers");
+console.log("Annual Flow of River: ".concat(River_Details.Annual_Flow) + "billion cu yd");
+var Basol_River = {
+    River_Name: "Basol River",
+    Total_Length_of_River: 31,
+    Annual_Flow: 138.9
+};
+console.log("\n Basol River Details");
+console.log("River Name: ".concat(Basol_River.River_Name));
+console.log("Total length of River : ".concat(Basol_River.Total_Length_of_River) + " kilometers");
+console.log("Annual Flow: ".concat(Basol_River.Annual_Flow) + " W/m2");
+var Nal_River = {
+    River_Name: "Nal River",
+    Total_Length_of_River: 960,
+    Annual_Flow: 146
+};
+console.log("\n Nal River Details");
+console.log("River Name: ".concat(Nal_River.River_Name));
+console.log("Total Length of River: ".concat(Nal_River.Total_Length_of_River) + " Kilometer");
+console.log("Annual Flow : ".concat(Nal_River.Annual_Flow) + "Million Acre Feet (MAF)");
+var Hub_River = {
+    River_Name: "Hub River",
+    Total_Length_of_River: 134,
+    Annual_Flow: 58
+};
+console.log("\n Hub River Details\n");
+console.log(" River Name : ".concat(Hub_River.River_Name));
+console.log("Total Length of River: ".concat(Hub_River.Total_Length_of_River) + " Kilometers");
+console.log("Annual Flow : ".concat(Hub_River.Annual_Flow) + "cubic miles (243 cubic km)");
+var Pakistan = {
+    Country_name: "Pakistan",
+    Capital_city: "Islamabad",
+    Population: 231.4,
+    Country: "India",
+    Capital: "New Delhi",
+    Popu: 1.408,
+    count: "Iran",
+    Cap: "Tehran",
+    pu: 87.92,
+    Try: "Bangladesh",
+    tal: "Dhaka",
+    la: 169.4
+};
+console.log("\n Pakistan Country Details \n");
+console.log("Country Name: ".concat(Pakistan.Country_name));
+console.log("Capital City Name :".concat(Pakistan.Capital_city));
+console.log("Population of Pakistan: ".concat(Pakistan.Population) + " million (2021)");
+console.log("\n India Country Details\n");
+console.log("Country Name :".concat(Pakistan.Country));
+console.log("Capital city of India: ".concat(Pakistan.Capital));
+console.log("Poputlation of India : ".concat(Pakistan.Popu) + " Billion (2021)");
+console.log("\n Iran Country Details\n");
+console.log("Country Name :".concat(Pakistan.count));
+console.log("Capital city of Iran: ".concat(Pakistan.Cap));
+console.log("Population of Iran : ".concat(Pakistan.pu) + " Million (2021)");
+console.log("\n Bangladesh Country Details\n");
+console.log("Country Name : ".concat(Pakistan.Try));
+console.log("Capital City of Bangladesh : ".concat(Pakistan.tal));
+console.log("Total Population Of Bangladesh: ".concat(Pakistan.tal) + " million(2021)");
