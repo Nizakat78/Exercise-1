94c60630ecf2131090546e255e4f11dfb71ab70b
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-02-29 21:33:44 +0500

    Add files via upload

---
+++ Ex23/conditional_test.ts
@@ -0,0 +1,36 @@
+let number = 8;
+// equal to 8 is predicat true.
+console.log(number ===8);
+// greater then 4 Is predicat true
+
+console.log( number >4)
+
+// this number less then 11 Is predicat True.
+ 
+console.log(number <11)
+
+//this number is less then or Equal to 8 Is Predicat True
+console.log(number >=8)
+
+// this is number is greatet then or equal to 8 Is predicat True
+console.log(number <=8)
+ 
+//this is number is not equal to 6 Is is Predicat true.
+
+console.log(number !==6)
+
+// Is number multipuly by 2 then equal to 16 Is predicat True.
+ console.log(number *2==8)
+
+ //In number is Divide by 2 then equal to 4 Is predicat true
+ console.log( number /2==8)
+
+
+// Is the string Apple is Equal to Orange ? Is predicat False.
+
+console.log (`apple`===`orange`)
+
+// this is number is string  8 equal Is predicat is False.
+console.log(number === `8`);
+
+//
