354869b9f52d5d5f2aa52a340c316b6404c7e7a4
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-01 23:06:50 +0500

    Add files via upload

---
+++ Ex28/ageelse.js
@@ -0,0 +1,17 @@
+var number = 2;
+if (number < 2) {
+    console.log("The person is a baby");
+}
+else if (number >= 2 && number < 4) {
+    console.log("The person is a toddler.");
+}
+else if (number >= 4 && number < 13) {
+    console.log("The person is a kid.");
+}
+else if (number >= 13 && number < 20) {
+    console.log("The person is a teenager.");
+}
+else if (number >= 20 && number < 65) {
+    console.log("The person is an adult.");
+}
+console.log("The person is an elder.");
