2ac750bafb25d1bb12d092972af1031efbd44f78
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-01 22:10:23 +0500

    Add files via upload

---
+++ Ex27/Alian_color.js
@@ -0,0 +1,33 @@
+//Version for a green alien
+var alaincolorgreen = "Green";
+if (alaincolorgreen === "Green") {
+    console.log("Congratulations! You just earned 5 points for shooting the Green alien.");
+}
+else if (alaincolorgreen === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the Yellow alien.");
+}
+else if (alaincolorgreen === "Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+//Version for a Yellow alien
+var alaincoloryellow = "yellow";
+if (alaincoloryellow === "Green") {
+    console.log("Congratulations! You just earned 5 points for shooting the Green alien.");
+}
+else if (alaincoloryellow === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the Yellow alien.");
+}
+else if (alaincoloryellow === "Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+//version for a Red 
+var alaincolorRed = " Red";
+if (alaincolorRed === "Green") {
+    console.log("Congratulations! You just earned 5 points for shooting the Green alien.");
+}
+else if (alaincolorRed === "Yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the Yellow alien.");
+}
+else if (alaincolorRed === " Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
---
+++ Ex27/Alian_color.ts
@@ -0,0 +1,37 @@
+//Version for a green alien
+let alaincolorgreen : string = "Green";
+
+if( alaincolorgreen === `Green`) {
+    console.log ("Congratulations! You just earned 5 points for shooting the Green alien.");
+}
+else if (alaincolorgreen === `yellow`) {
+    console.log("Congratulations! You just earned 10 points for shooting the Yellow alien.")
+}
+else if (alaincolorgreen ===`Red`) {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.")
+}
+
+//Version for a Yellow alien
+let  alaincoloryellow : string ="yellow";
+
+if (alaincoloryellow === "Green") {
+    console.log("Congratulations! You just earned 5 points for shooting the Green alien.");
+}
+else if  (alaincoloryellow === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the Yellow alien.");
+}
+else if (alaincoloryellow === "Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+
+//version for a Red 
+let alaincolorRed :  string =" Red"
+if (alaincolorRed === "Green") {
+    console.log("Congratulations! You just earned 5 points for shooting the Green alien.")
+}
+else if (alaincolorRed === "Yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the Yellow alien.")
+}
+else if (alaincolorRed === " Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.")
+}
