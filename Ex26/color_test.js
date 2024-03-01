01ea28afc0346660f4d8e90e9fe63ed02884774c
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-01 20:46:09 +0500

    Add files via upload

---
+++ Ex26/color_test.js
@@ -0,0 +1,16 @@
+// Version that runs the if block
+var passblock = "green";
+if (passblock === "green") {
+    console.log("You just earned 0 points for shooting the green alien.");
+}
+else {
+    console.log("You just earned 10 points for shooting an alien of a different color.");
+}
+// version that runs the else block
+var ntpassblock = "Red";
+if (ntpassblock === "green") {
+    console.log(" You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("You just earned 15 points for shooting an alien of a different color.");
+}
---
+++ Ex26/color_test.ts
@@ -0,0 +1,18 @@
+// Version that runs the if block
+
+let  passblock : String = "green";
+
+if (passblock === `green`) {
+    console.log ("You just earned 5 points for shooting the green alien.")
+ } else {
+    console.log("You just earned 10 points for shooting an alien of a different color.")
+ }
+
+ // version that runs the else block
+let ntpassblock : string ="Red";
+if (ntpassblock === `green` ) {
+    console.log (` You just earned 5 points for shooting the green alien.`)
+}
+else  {
+    console.log(`You just earned 15 points for shooting an alien of a different color.`)
+}
