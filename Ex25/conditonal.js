5e7a3d68c00b732699351f467f6d47b76006ae18
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-01 20:21:11 +0500

    Add files via upload

---
+++ Ex25/conditonal.js
@@ -0,0 +1,10 @@
+//Version that passes the if test
+var alien_color = "Green";
+if (alien_color === "Green") {
+    console.log("Congratulations! you earned 5 points ");
+}
+// Version that fails the if test
+var fail_color = "Red";
+if (fail_color === "Green") {
+    console.log("Congratulation! your earned 5 points");
+}
---
+++ Ex25/conditonal.ts
@@ -0,0 +1,14 @@
+//Version that passes the if test
+
+let  alien_color  : string  = "Green";
+
+if  (alien_color === `Green`) {
+    console.log(`Congratulations! you earned 5 points `) }
+
+    // Version that fails the if test
+
+
+    let fail_color : string  = "Red";
+if (fail_color === `Green`) {
+    console.log(`Congratulation! you earned 5 points`)
+}
