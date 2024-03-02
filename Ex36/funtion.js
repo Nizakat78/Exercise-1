00f1ef358c8146511a72faf64a91747e4ad626eb
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-02 22:56:19 +0500

    Add files via upload

---
+++ Ex36/funtion.js
@@ -0,0 +1,6 @@
+// Define the function make_shirt
+function make_shirt(size, message) {
+    console.log("T-shirt size: ".concat(size, ", Message ").concat(message));
+}
+// Call the function with specific size and message
+make_shirt("Medium", "Hello, World!");
