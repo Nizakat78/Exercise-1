06f9978f83e475967912573602befdaa892f9874
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-02 22:26:47 +0500

    Add files via upload

---
+++ Ex33/number.ts
@@ -0,0 +1,32 @@
+// Store the numbers 1 through 9 in an array
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Loop through the array
+for(let number of numbers) {
+    if (number ===1 ) {
+        console.log(`${number}st`);
+    }
+    else if (number ===2 ) {
+        console.log(`${number}nd`);
+    }
+    else if (number === 3) {
+        console.log(`${number}rd`)
+    }
+    else if(number === 4) {
+        console.log(`${number}th`)
+    }
+    else if(number === 5) {
+        console.log(`${number}th`);
+    }
+    else if(number=== 6) {
+        console.log(`${number}th`)
+    }
+    else if(number === 7) {
+        console.log(`${number}th`)
+    }
+    else if (number === 8){
+        console.log(`${number}th`)
+    }
+    else if(number === 9) {
+        console.log(`${number}th`)
+    }
+}
