eff392d134e58ae9ef22256e4e3b2ec95f94aeea
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-04 11:29:11 +0500

    Add files via upload

---
+++ Ex40/Album.js
@@ -0,0 +1,20 @@
+// Define the function make_album with optional tracks parameter
+function make_album(artist, title, tracks) {
+    var album = {
+        artist: artist,
+        title: title
+    };
+    // Add tracks to the album if provided
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
+}
+// Make three dictionaries representing different albums
+var album1 = make_album("Name : Ahmed", " Name : Happy");
+var album2 = make_album("Name : kamran", " Name : Udas", 12); // Including the number of tracks
+var album3 = make_album("Name :Pervaiz", " Name : King");
+// Print each return value to show that objects are storing the album information correctly
+console.log(album1);
+console.log(album2);
+console.log(album3);
