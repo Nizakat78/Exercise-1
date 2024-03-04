701315f4e2b6f77e191e8df959e16f816eec5a8c
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-04 10:56:15 +0500

    Add files via upload

---
+++ Ex39/city_name.js
@@ -0,0 +1,12 @@
+// Define the function city_country
+function City_name(city, country) {
+    return "".concat(city, ", is in ").concat(country);
+}
+// Call the function with at least three city-country pairs
+var city1 = City_name("Lahore", "Pakistan");
+var city2 = City_name("New York", "USA");
+var city3 = City_name("Pairs", "France");
+// Print the values returned by the function
+console.log(city1);
+console.log(city2);
+console.log(city3);
---
+++ Ex39/city_name.ts
@@ -0,0 +1,16 @@
+// Define the function city_country
+
+function City_name( city : string, country : string): string {
+    return `${city}, is in ${country}`
+}
+
+
+// Call the function with at least three city-country pairs
+let city1 = City_name ("Lahore", "Pakistan");
+let city2= City_name  ("New York","USA");
+let city3 =City_name ("Pairs", "France");
+
+// Print the values returned by the function
+console.log(city1)
+console.log(city2)
+console.log(city3)
