2581b5bc190f178da441098b159ca91ca2cc840a
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-04 11:43:39 +0500

    Add files via upload

---
+++ Ex41/Magicians.js
@@ -0,0 +1,10 @@
+// Define the function show_magicians
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magicians);
+    }
+}
+// Make an array of magician’s names
+var magician_name = ["Daod", "Kamran", "Razi", "Ravi", "Ali"];
+show_magicians(magician_name);
---
+++ Ex41/Magicians.ts
@@ -0,0 +1,9 @@
+// Define the function show_magicians
+function show_magicians( magicians : string[]): void{
+    for(let magician of  magicians){
+        console.log(magicians);
+    }
+}
+// Make an array of magician’s names
+let magician_name: string[]=["Daod","Kamran","Razi", "Ravi","Ali"]
+show_magicians(magician_name);
